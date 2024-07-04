062e694bb75de1b05634cb13ecd6ef228ceacfa7
VinhHH <vinhhh@kozo-japan.com>
2024-07-04 08:48:08 +0700

    setup react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './layouts/Sidebar';
-import EmployeeSearch from './pages/Employee/EmployeeSearch';
+import EmployeeSearch from './pages/Employee/Search';
+import EmployeeCreate from './pages/Employee/Create';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           {/* Các component con sẽ hiển thị trong sidebar-content */}
           <Routes>
             <Route path="/employees/search" element={<EmployeeSearch />} />
+            <Route path="/employees/create" element={<EmployeeCreate />} />
             {/* Thêm các route khác nếu cần */}
           </Routes>
         </Sidebar>
